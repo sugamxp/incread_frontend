ac38f705b6e491341b284012da824d31afcda117
Sugam Devare <sugamxp@gmail.com>
2020-02-28 22:16:52 +0530

    redux-dev-remove

--- src/redux/store.js
+++ src/redux/store.js
@@ -10,12 +10,12 @@ const store = createStore(
   rootReducer,
   initialState,
 
-  // compose(applyMiddleware(...middleware))
+  compose(applyMiddleware(...middleware))
 
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  // compose(
+  //   applyMiddleware(...middleware),
+  //   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  // )
 );
 
 export default store;
